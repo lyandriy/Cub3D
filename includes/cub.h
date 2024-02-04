623d5b2e3441d93df49239c8cb0e18a311a68d9a
Lyudmyla Andriychyk Fetsenets <lyandriy@c2r7s5.42madrid.com>
2024-02-04 19:14:39 +0100

    first commit

---
+++ includes/cub.h
@@ -0,0 +1,321 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyandriy <lyandriy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/22 17:59:01 by samusanc          #+#    #+#             */
+/*   Updated: 2023/12/01 17:16:56 by samusanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB_H
+# define CUB_H
+# include <mlx.h>
+# include <math.h>
+# include <ft_math.h>
+# include <stdio.h>
+# include <libft.h>
+# include <ft_perror.h>
+# include <ft_utils.h>
+# include <bresenham.h>
+# define WIDTH 640
+# define HEIGHT 480
+# define ZOOM_L 7
+# define ZOOM_M 14
+# define ZOOM_S 35
+# define COLOR_W 0x8CFFFFFF
+# define COLOR_E 0x8C000000
+# define COLOR_D 0x8C00FF00
+# define COLOR_P 0x8CFF0000
+# define PI 3.1415926535
+# define DR 0.0174533
+# define SPEED 0.1
+# define OFF 0
+# define ON 1
+# define FRONT 1
+# define SIDE 0
+# ifndef BONUS
+#  define BONUS 0
+# endif
+
+typedef struct s_tex{
+	int		**tex;
+	size_t	size;
+}				t_tex;
+
+typedef struct s_spr{
+	size_t	x;
+	size_t	y;
+	int		blink;
+	int		frame;
+	size_t	next_frame;
+	size_t	next_blink;
+}				t_spr;
+
+typedef struct s_img{
+	void	*img;
+	char	*data_addr;
+	int		*pixel_addr;
+	int		bits_per_pixel;
+	int		line_size;
+	int		endian;
+	double	transparency;
+	int		width;
+	int		height;
+}				t_img;
+
+/*
+ *		map notation
+ *		1 wall
+ *		0 empty
+ *		2 player facing north
+ *		3 player facing south
+ *		4 player facing east
+ *		5 player facing west
+ *		6 door close
+ *		7 door open
+ *		8 player in door
+ *		9 space in map
+ *		10 ghost
+ */
+typedef struct s_cub{
+	size_t	**map;
+	size_t	map_width;
+	size_t	map_height;
+	float	**screen;
+	t_img	*no_texture;
+	t_img	*so_texture;
+	t_img	*ea_texture;
+	t_img	*we_texture;
+	t_img	*black;
+	t_tex	*no_t;
+	t_tex	*so_t;
+	t_tex	*ea_t;
+	t_tex	*we_t;
+	t_tex	*black_t;
+	int		color_ground;
+	int		color_sky;
+	int		door;
+	void	*mlx;
+	void	*win;
+	t_img	*game;
+	t_img	*cam;
+	t_img	*atm;
+	t_img	*hud_o;
+	t_img	*hud_c;
+	t_img	*hand;
+	t_img	*hand_m;
+	t_img	*hand_l;
+	t_img	*noise1;
+	t_img	*noise2;
+	t_img	*noise3;
+	t_img	*noise4;
+	t_img	*noise5;
+	t_img	*noise6;
+	t_img	*minimap;
+	int		camera_speed;
+	float	player_px;
+	float	player_py;
+	float	player_dx;
+	float	player_dy;
+	float	player_d2x;
+	float	player_d2y;
+	float	player_a;
+	int		handx;
+	int		handy;
+	int		mousex;
+	int		mousey;
+	int		mouse_press;
+	int		minimap_zoom;
+	int		fisheye;
+	int		cam_status;
+	size_t	frame;
+	int		cam_animation;
+	t_img	*miniscreen_s;
+	t_img	*miniscreen_m;
+	t_img	*nina_cam[3][5];
+	t_img	*nina_ncam[5];
+	t_tex	*t_nina_cam[3][5];
+	t_tex	*t_nina_ncam[5];
+	t_spr	**sprites;
+}				t_cub;
+
+typedef struct s_win{
+	void	*mlx;
+	void	*win;
+	t_img	*result;
+}				t_win;
+
+typedef struct s_ray{
+	float	x;
+	float	y;
+	float	angle;
+	float	distance;
+	float	color;
+	float	shadow;
+	int		side;
+}				t_ray;
+
+//============================================================================//
+//================================MATH========================================//
+int		ft_last_parse(t_cub *cub);
+float	ft_random(float n, float min, float max);
+void	free_split(char **input);
+double	ft_ds(double xi, double xf, double yi, double yf);
+double	angle_to_radian(double angle);
+double	get_angle(double angle);
+double	get_decimal(double x);
+double	ft_abs2(double x);
+float	ft_fit_char(float n, float min, float max);
+float	ft_random(float n, float min, float max);
+double	closer_int(double n);
+void	put_deltas(t_cub *cub);
+//============================================================================//
+//=================================MLX========================================//
+t_dwu	dwu_tmp(size_t total_rays, int side, int win);
+int		get_pixel_img(t_img *img, int x, int y);
+int		cub_open_utils(t_cub *cub);
+void	ft_put_pixel(t_img *img, int x, int y, int color);
+void	ft_fill_img(t_img *img, int color);
+t_img	*ft_init_img(void *mlx, int width, int height);
+void	draw_square(t_img *img, t_square sq);
+void	fill_img_sky_n_ground(t_img *img, int color1, int color2, int status);
+void	make_img_translucent(t_img *img, double o);
+t_tex	*img_to_tex(t_img *img);
+double	get_opacity(int color);
+int		get_flat_color(int color);
+void	make_img_translucent(t_img *img, double o);
+//============================================================================//
+//================================ERROR=======================================//
+void	*ft_perror(char *error);
+//============================================================================//
+//===============================PARSING======================================//
+t_cub	*map_parsing(char *file);
+int		check_char_of_str(t_cub *cub, char **archive, int *count);
+int		fill_struct(t_cub *cub, char *line);
+int		copy_map(t_cub *cub, char **archive);
+int		check_maps(t_cub *cub);
+int		copy_archive(int fd, char **archive, int height_archive);
+int		count_height_archive(int fd, int *height_archive);
+int		init_cub(t_cub	*cub);
+int		ft_free_struct(t_cub *cub);
+void	free_archive(char **archive);
+t_img	*ft_open_img(void *mlx, char *path);
+int		separate(t_cub *cub, char **archive);
+size_t	ft_strlcpys(char *dst, const char *src, size_t dstsize);
+int		identifier_color(char *line, int *color, int *count);
+int		check_wall(t_cub *cub, size_t *count_line, size_t *count_int);
+int		open_all_img(t_cub *cub);
+int		img_to_tex_all(t_cub *cub);
+int		resize_map(t_cub *cub);
+int		put_sprites(t_cub *cub);
+void	map_parsing_aux(t_cub *cub);
+int		cub_open_utils(t_cub *cub);
+t_img	*ft_init_img(void *mlx, int width, int height);
+//============================================================================//
+//===============================MINIMAP======================================//
+void	set_zero_minimap(char **minimap, int size);
+void	write_minimap(char **minimap, int size);
+char	**alloc_minimap(int size);
+void	put_chunks_minimap(char **minimap, int i, int j, char c);
+void	put_walls_minimap(char **minimap, int x, int y, t_cub *cub);
+int		get_color_minimap(char c);
+void	put_pixels_minimap(char **minimap, t_cub *cub);
+void	draw_minimap(t_cub *cub);
+void	free_map(int tam_line, char **map, int fd);
+//============================================================================//
+//===============================MINIMAP======================================//
+void	set_zero_minimap(char **minimap, int size);
+void	write_minimap(char **minimap, int size);
+char	**alloc_minimap(int size);
+void	put_chunks_minimap(char **minimap, int i, int j, char c);
+void	put_walls_minimap(char **minimap, int x, int y, t_cub *cub);
+int		get_color_minimap(char c);
+void	put_pixels_minimap(char **minimap, t_cub *cub);
+void	draw_minimap(t_cub *cub);
+//============================================================================//
+//=================================CUB========================================//
+t_stat1	tmp_status(float ray_a, float *ds, int m, float last_distance);
+void	fill_img_sky_n_ground(t_img *img, int color1, int color2, int status);
+void	free_split(char **input);
+void	write_map(t_cub *cub);
+void	draw_direction(t_cub *cub);
+void	draw_minimark(t_cub *cub);
+void	draw_walls_utlis_8(t_cub *cub, t_ray ray, t_dw *dw);
+void	draw_walls_utlis_7(t_cub *cub, t_ray ray, t_dw *dw);
+void	draw_walls_utlis_6(t_cub *cub, t_ray ray, t_dw *dw);
+void	draw_walls_utlis_5(t_cub *cub, t_ray ray, t_dw *dw);
+void	draw_walls_utlis_4(t_cub *cub, t_ray ray, t_dw *dw);
+void	draw_walls_utlis_3(t_cub *cub, t_ray ray, t_dw *dw);
+void	draw_walls_utlis_2(t_cub *cub, t_ray ray, t_dw *dw);
+void	draw_walls_utlis_1(t_cub *cub, t_ray ray, t_dw *dw, size_t actual_ray);
+void	draw_walls_utlis_11(t_cub *cub, t_ray ray, t_dw *dw);
+void	draw_walls_utlis_13(t_cub *cub, t_ray ray, t_dw *dw);
+void	draw_walls_utlis_12(t_cub *cub, t_ray ray, t_dw *dw);
+void	draw_walls_utlis_10(t_cub *cub, t_ray ray, t_dw *dw);
+void	draw_walls_utlis_9(t_cub *cub, t_ray ray, t_dw *dw);
+void	draw_walls(t_cub *cub, t_ray ray, size_t actual_ray, t_dwu du);
+t_p		default_ray(t_cub *cub);
+t_p		calculate_cam_plane(t_ray ray, t_cub *cub);
+t_ray	calculate_ray(t_ray tmp_ray, t_cub *cub, int color);
+t_ray	make_tmp_ray(float x, float y, float angle, t_cub *cub);
+t_p		ray_to_point(t_ray ray);
+int		get_next_status(t_cub *cub, t_stat1 b);
+void	init_ray_loop(t_cub *cub, t_rayc *ray);
+void	init_ray_casting_loop(t_cub *cub, t_rayc *ray);
+void	ray_casting_loop(t_cub *cub, t_rayc *ray);
+void	calculate_side_util(t_cub *cub, t_rayc *ray);
+void	ray_map_draw_rays(t_cub *cub);
+void	draw_ray_map(t_cub *cub);
+void	draw_cross(t_cub *cub);
+t_win	tmp_win(void *mlx, void *win, t_img *result);
+void	put_noise(t_cub *cub);
+void	ray_caster(t_cub *cub);
+void	sprites_utils_16(t_cub *cub, t_sp *sp, t_tex *tex);
+void	sprites_utils_15(t_cub *cub, t_sp *sp, t_tex *tex);
+void	sprites_utils_14(t_cub *cub, t_sp *sp, t_tex *tex);
+void	sprites_utils_13(t_cub *cub, t_sp *sp, t_tex *tex);
+void	sprites_utils_12(t_cub *cub, t_sp *sp, t_tex *tex);
+void	sprites_utils_11(t_cub *cub, t_sp *sp, t_tex *tex);
+void	sprites_utils_10(t_cub *cub, t_sp *sp, t_tex *tex);
+void	sprites_utils_9(t_cub *cub, t_sp *sp, t_tex *tex);
+void	sprites_utils_8(t_cub *cub, t_sp *sp, t_tex *tex);
+void	sprites_utils_7(t_cub *cub, t_sp *sp);
+void	sprites_utils_6(t_cub *cub, t_sp *sp);
+void	sprites_utils_5(t_cub *cub, t_sp *sp);
+void	sprites_utils_4(t_cub *cub, t_sp *sp, t_tex *tex);
+void	sprites(t_cub *cub, float sx, float sy, t_tex *tex);
+t_tex	*get_tex(t_cub *cub, t_spr *tmp);
+void	ft_put_sprites(t_cub *cub);
+void	start_cub(t_cub *cub);
+//============================================================================//
+//===============================CONTROLS=====================================//
+void	close_x(void *param);
+void	open_doors(t_cub *cub);
+void	close_doors(t_cub *cub);
+void	minimap_zoom(t_cub *cub, int key);
+void	hand_loop(t_cub *cub);
+void	move_left(t_cub *cub, float *starty, float *startx);
+void	move_right(t_cub *cub, float *starty, float *startx);
+void	move_up(t_cub *cub, float *starty, float *startx);
+void	move_down(t_cub *cub, float *starty, float *startx);
+void	put_deltas(t_cub *cub);
+void	rotate_view_left(t_cub *cub);
+void	rotate_view_rigth(t_cub *cub);
+void	move_player(int key, t_cub *cub);
+void	increase_dpi(t_cub *cub, int key);
+void	turn_camera(t_cub *cub);
+void	key_press(int key, void *param);
+void	frame(void *param);
+void	mouse_location(int x, int y, void *param);
+void	mouse_press(int key, int x, int y, void *param);
+void	mouse_release(int key, int x, int y, void *param);
+void	start_controls(t_cub *cub);
+//============================================================================//
+//=================================MAIN=======================================//
+void	start_loop(t_cub *cub);
+int		main(int argc, char **argv);
+
+#endif
---
+++ includes/ft_utils.h
@@ -0,0 +1,171 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samusanc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 12:20:28 by samusanc          #+#    #+#             */
+/*   Updated: 2023/11/30 22:03:27 by samusanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_UTILS_H
+# define FT_UTILS_H
+# include <stdio.h>
+
+typedef struct s_put_walls{
+	int	i;
+	int	j;
+	int	minx;
+	int	x2;
+	int	y2;
+}				t_put_walls;
+
+typedef struct s_square{
+	int	x;
+	int	y;
+	int	len;
+	int	color;
+}				t_square;
+
+typedef struct s_sg{
+	int		i;
+	int		j;
+	float	m;
+	int		color;
+	int		color1;
+	int		color2;
+	float	x;
+	int		cam;
+	int		ground;
+}				t_sg;
+
+typedef struct s_rayc{
+	double		x;
+	double		y;
+	double		anglei;
+	double		ray_dx;
+	double		ray_dy;
+	double		angle_chunk;
+	double		last_distance;
+	float		fogxf;
+	float		fogyf;
+	float		fog_ds;
+	float		actual_ds;
+	float		winx;
+	float		winy;
+	float		ray_a;
+	float		ray_proyection;
+	int			ray;
+	int			angle;
+	int			color2;
+	int			status1;
+	int			chunks;
+	int			win;
+}				t_rayc;
+
+typedef struct s_stat{
+	double	x;
+	double	y;
+	double	ray_dx;
+	double	ray_dy;
+	float	ray_proyection;
+	int		status1;
+}				t_stat;
+
+typedef struct s_stat1{
+	float	last_distance;
+	float	ray_a;
+	float	*ds;
+	int		m;
+}				t_stat1;
+
+typedef struct s_sp{
+	float	x;
+	float	y;
+	float	angle;
+	float	angle2;
+	float	up;
+	float	down;
+	float	screenx;
+	float	ds;
+	float	distance;
+	float	offset_up;
+	float	offset_down;
+	float	offset_left;
+	float	offset_right;
+	float	offup;
+	float	offdo;
+	float	offset;
+	float	chunk;
+	float	offl;
+	float	offr;
+	float	txt_i;
+	float	txt_f;
+	float	y2;
+	float	new_size;
+	float	start_point;
+	float	end_point;
+	float	real_wall_ds;
+	float	z;
+	float	chunk2;
+	float	shadow;
+	float	tmp1;
+	int		side;
+	int		i;
+	int		j;
+	int		r_wall_len;
+	int		k;
+	int		initial;
+	int		final;
+	int		size;
+	int		color;
+	int		tmp;
+	int		u;
+	int		zz;
+	int		zx;
+	int		translucid;
+	size_t	wall_len;
+}				t_sp;
+
+typedef struct s_dw{
+	int		side;
+	int		win;
+	int		total_rays;
+	int		i;
+	int		j;
+	int		k;
+	int		start_x;
+	int		len;
+	int		offset_up;
+	int		offset_down;
+	int		fog;
+	int		actual_chunk;
+	int		size;
+	int		real_wall_ds;
+	int		**tex;
+	float	pixel;
+	float	start_point;
+	float	end_point;
+	float	x;
+	float	color_ground_mix;
+	double	distance;
+	size_t	wall_len;
+}				t_dw;
+
+typedef struct s_dwu{
+	int		side;
+	int		win;
+	size_t	total_rays;
+}				t_dwu;
+
+typedef struct s_cii{
+	size_t	ii;
+	size_t	jj;
+	double	multix;
+	double	multiy;
+	int		color2;
+}				t_cii;
+
+#endif
